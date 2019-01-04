953d561736ebce87788f2970daf82b38f20403f4
krzyc3d <krzych3d@wp.pl>
2019-01-04 14:42:18 +0100

    adding services to heroApp

---
+++ angular/src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HEROES } from './mock-heroes'
+import { Hero } from './hero'
+import { Observable, of} from 'rxjs'
+import { MessageService } from './messages.service'
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
+  getHeroes(): Observable< Hero[]>{
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+}
